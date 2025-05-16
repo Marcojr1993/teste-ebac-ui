a4bb7cd738afbe27a109657f810203adc231db2a
MarcoJr <marco.suelio@hotmail.com>
2025-05-16 00:06:05 -0300

    Listas e tarefa

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+
+describe('Funcionalidade: Produtos', () => {
+
+  beforeEach(() => {
+
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+
+    });
+
+    afterEach(() => {
+
+        cy.screenshot()
+
+    });
+
+    
+    it('Deve selecionar um produto da Lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(5)
+            .contains('Argus All-Weather Tank')
+            .click()
+            cy.get('#tab-title-description > a').should('contain','Descrição')
+
+
+        
+    });
+});
