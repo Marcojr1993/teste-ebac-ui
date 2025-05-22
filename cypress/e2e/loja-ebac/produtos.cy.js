56a700732b9fb1b5e1d3620d9de60298bdc3de54
MarcoJr <marco.suelio@hotmail.com>
2025-05-21 23:10:44 -0300

    ultimo do modulo

--- cypress/e2e/loja-ebac/produtos.cy.js
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import produtosPages from "../../support/pages-objects/produtos.pages";
 import produtosPage from "../../support/pages-objects/produtos.pages";
 
 
@@ -29,10 +30,24 @@ describe('Funcionalidade: Produtos', () => {
 
     });
 
-    it.only('Deve adicionar um produto ao carrinho ', () => {
+    it('Deve adicionar um produto ao carrinho ', () => {
            let qtd = 7
             produtosPage.buscarProduto('Abominable Hoodie')
             produtosPage.addProdutoCarrinho('M','Red', qtd)
             cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho,
+                dados[2].cor,
+                dados[2].quantidade)
+
+            cy.get('.woocommerce-message').should('contain',dados[2].nomeProduto )
+
+        })
+
     });
 });
